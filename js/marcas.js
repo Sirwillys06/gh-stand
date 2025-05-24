3822275ec779f6447d43c90f2aecf09d574bbdec
Sirwillys06 <whurtado180@gmail.com>
2025-05-24 10:44:06 -0500

    mejora carrusel de cliente

--- js/marcas.js
+++ js/marcas.js
@@ -3,13 +3,27 @@ const clientesSwiper = new Swiper('.clientesSwiper', {
   slidesPerView: 'auto',
   spaceBetween: 40,
   grabCursor: true,
+  centeredSlides: true, // Centra los slides activos
   autoplay: {
     delay: 2500,
     disableOnInteraction: false
   },
   breakpoints: {
-    320: { slidesPerView: 2 },
-    768: { slidesPerView: 4 },
-    1024: { slidesPerView: 6 }
+    320: { 
+      slidesPerView: 2,
+      spaceBetween: 20 
+    },
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    768: { 
+      slidesPerView: 4,
+      spaceBetween: 40 
+    },
+    1024: { 
+      slidesPerView: 6,
+      spaceBetween: 50 
+    }
   }
 });
